a4eb54bf6d2794aac85bf50db9ddf95671942704
dgadrian05 <dgadrian05@gmail.com>
2025-01-16 21:27:43 +0200

    no message

---
+++ BasicAlgorithms/src/main/java/interclassingVectors/interclasare.java
@@ -0,0 +1,14 @@
+package interclassingVectors;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class interclasare {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        File input=new File("src/main/resources/interclasare.txt");
+        Scanner citesc = new Scanner(input);
+        File output=new File("src/main/resources/outputInterClasare.txt");
+        System.out.println(input.);
+    }
+}
