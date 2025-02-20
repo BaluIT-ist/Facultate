101ddfb7f1f7966ba4742c18af1542b637f8c7a8
dgadrian05 <dgadrian05@gmail.com>
2025-02-20 23:45:04 +0200

    Interclasarea a doua siruri / Algorithm

--- BasicAlgorithms/src/main/java/interclassingVectors/interclasare.java
+++ BasicAlgorithms/src/main/java/interclassingVectors/interclasare.java
@@ -4,11 +4,53 @@ import java.io.*;
 import java.util.Scanner;
 
 public class interclasare {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
 
-        File input=new File("src/main/resources/interclasare.txt");
-        Scanner citesc = new Scanner(input);
-        File output=new File("src/main/resources/outputInterClasare.txt");
-        System.out.println(input.);
+
+            Scanner sc = new Scanner(new File("src\\main\\resources\\interclassingVectors\\interclasare.in"));
+            PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("src\\main\\resources\\interclassingVectors\\interclasare.out")));
+
+            int n = sc.nextInt();
+            int [] a = new int[n];
+            for(int i = 0;i<n;i++)
+            {
+                a[i]=sc.nextInt();
+            }
+
+            int m = sc.nextInt();
+            int [] b = new int[m];
+            for (int  i = 0; i<m; i++)
+            {
+                b[ i]=sc.nextInt();
+            }
+
+            int [] c = new int [n+m];
+            int i = 0,j=0,k=0;
+
+            while (i<n && j<m) {
+
+                if(a[i]<b[j]) {
+                    c[k++]=a[i++];
+                }
+                else {
+                   c[k++]=b[j++];
+                }
+            }
+
+            while(i<n) c[k++] = a[i++];
+            while(j<m) c[k++] = b[j++];
+
+            for(i=0;i<c.length;i++){
+                pw.print(c[i]+ " ");
+                if((i+1)% 10 == 0) pw.println();
+            }
+
+            sc.close();
+            pw.close();
+
+
+
     }
+
+
 }
